b2904713aff56cdb10263403cddc1ad338e54507
kubasikora <283418@pw.edu.pl>
2020-12-29 22:02:27 +0100

    example user filtering

--- betting/api.py
+++ betting/api.py
@@ -5,6 +5,7 @@ app_name = "betting"
 
 urlpatterns = [
     path('bets', views.BetListRESTView.as_view(), name='rest_bet_list'),
+    path('bets/mine', views.UserBetsListRESTView.as_view(), name='rest_user_bets_list'),
     path('leagues', views.LeagueListRESTView.as_view(), name='rest_league_list'),
     path('participants', views.ParticipantListRESTView.as_view(), name='rest_participants_list')
 ]
--- betting/views.py
+++ betting/views.py
@@ -5,12 +5,23 @@ from betting import models, serializers
 
 # rest endpoint views
 
-"""Widok listy wszystkich dostępnych zawodników/drużyn"""
+"""Widok listy wszystkich dostępnych zakładów"""
 class BetListRESTView(generics.ListAPIView):
     queryset = models.Bet.objects.all()
     serializer_class = serializers.BetSerializer
     schema = AutoSchema(operation_id_base='TBD', tags=['betting'])
 
+
+"""Widok listy wszystkich dostępnych zakładów konkretnego użytkownika"""
+class UserBetsListRESTView(generics.ListAPIView):
+    serializer_class = serializers.BetSerializer
+    schema = AutoSchema(operation_id_base='user_bets', tags=['betting'])
+
+    """ Żeby filtrować wynik po użytkowniku trzeba nadpisać funkcję get_queryset
+        i tam można działać cuda
+    """
+    def get_queryset(self):
+        return models.Bet.objects.filter(participant__user=self.request.user)
 
 class LeagueListRESTView(generics.ListAPIView):
     queryset = models.League.objects.all()
